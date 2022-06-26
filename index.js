38a798fdfed74f43498e121b63cffe2c9d9b1344
Eric Chin <ericychin94@gmail.com>
2022-06-26 01:27:33 -0700

    POST route now handles addition of new note to db

--- index.js
+++ index.js
@@ -79,21 +79,21 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
     const body = request.body;
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: "content missing"
         });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId()
-    };
+        date: new Date()
+    });
 
-    notes = notes.concat(note);
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
 });
 
 const unknownEndpoint = (request, response) => {
