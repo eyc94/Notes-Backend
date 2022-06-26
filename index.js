9f18d169b431f05e5e5d9b4da2b3628e15192edb
Eric Chin <ericychin94@gmail.com>
2022-06-26 01:29:33 -0700

    Changed route to handle single fetch resource to take from db

--- index.js
+++ index.js
@@ -51,14 +51,9 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
