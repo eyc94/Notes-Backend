c2dda37061fe6e0a53ac0fd0b9ca31ef4d5a0d2f
Eric Chin <ericychin94@gmail.com>
2022-06-26 17:11:17 -0700

    Changed the delete method to use data from db

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const Note = require("./models/note");
 
 const app = express();
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("build"));
+
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
@@ -63,10 +64,11 @@ app.get("/api/notes/:id", (request, response, next) => {
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
