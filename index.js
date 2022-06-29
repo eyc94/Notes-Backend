faa29e44ea5b9b038c3d92d9252d760edcc45572
Eric Chin <ericychin94@gmail.com>
2022-06-28 20:24:51 -0700

    Added simplified code for index.js on top

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const app = require("./app");   // Actual Express application.
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`);
+});
+
+
+
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
