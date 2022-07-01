875e4ee5e2e6dbd1d39a7fbe2eb6c2926a381411
Eric Chin <ericychin94@gmail.com>
2022-07-01 00:21:49 -0700

    Added hardcoded initial notes and a beforeEach function to initialize a db before every test

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
 const api = supertest(app);
+const Note = require("../models/Note");
+
+const initialNotes = [
+    {
+        content: "HTML is Easy",
+        date: new Date(),
+        important: false
+    },
+    {
+        content: "Browser can execute only Javascript",
+        date: new Date(),
+        important: true
+    }
+];
+
+beforeEach(async () => {
+    await Note.deleteMany({});
+    let noteObject = new Note(initialNotes[0]);
+    await noteObject.save();
+    noteObject = new Note(initialNotes[1]);
+    await noteObject.save();
+});
 
 test("Notes are returned as JSON", async () => {
     await api
