640c3bece73f88344879029526dfc61ca4766ce7
Eric Chin <ericychin94@gmail.com>
2022-07-01 01:11:50 -0700

    Added test for adding one note and testing it's actually there

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -43,6 +43,25 @@ test("A specific note is within the returned notes", async () => {
     expect(contents).toContain("Browser can execute only Javascript");
 });
 
+test("A valid note can be added", async () => {
+    const newNote = {
+        content: "async/await simplifies making async calls",
+        important: true
+    };
+
+    await api
+        .post("/api/notes")
+        .send(newNote)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/notes");
+    const contents = response.body.map(r => r.content);
+
+    expect(response.body).toHaveLength(initialNotes.length + 1);
+    expect(contents).toContain("async/await simplifies making async calls");
+});
+
 afterAll(() => {
     mongoose.connection.close();
 });
