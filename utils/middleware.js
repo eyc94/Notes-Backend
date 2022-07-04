0bf6e908a7355112a5393c714c3774eb0f782498
Eric Chin <ericychin94@gmail.com>
2022-07-04 02:42:50 -0700

    Added error handler for invalid token

--- utils/middleware.js
+++ utils/middleware.js
@@ -19,6 +19,8 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: "Malformatted ID" });
     } else if (error.name === "ValidationError") {
         return response.status(400).send({ error: error.message });
+    } else if (error.name === "JsonWebTokenError") {
+        return response.status(401).json({ error: "Invalid token" });
     }
 
     next(error);
